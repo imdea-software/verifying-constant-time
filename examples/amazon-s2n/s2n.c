3b8331bd79dd848e0b7da73bff6c198d7a1fba3c
François Dupressoir <fdupress@gmail.com>
2015-08-19 18:12:09 +0200

    Temporary entry point for MEE-TLS-CBC
    Compilation requires you to follow the s2n build instructions:
    https://github.com/awslabs/s2n/blob/master/docs/USAGE-GUIDE.md
    
    We will later need to replace the core crypto algorithms with constant-time ones.

---
+++ examples/amazon-s2n/s2n.c
@@ -0,0 +1,21 @@
+#include "../smack.h"
+
+#include "tls/s2n_record.h"
+
+int s2n_verify_cbc_wrapper(struct s2n_connection *conn, struct s2n_hmac_state *hmac, struct s2n_blob *decrypted) {
+  /* Argument addresses are public (in case they are checked against NULL */
+  public_in(region_of_var(conn));
+  public_in(region_of_var(hmac));
+  public_in(region_of_var(decrypted));
+
+  /* A lot more of these are probably needed */
+  public_in(mem_region(decrypted->data,decrypted->allocated));
+
+  /* These are the only two we care about with this function as entry
+     point, but more will be needed if we want to us e this result
+     modularly to prove things about TLS as a whole */
+  declassified_return();
+  declassified_out(mem_region(decrypted->data,decrypted->size));
+
+  s2n_verify_cbc_wrapper(conn,hmac,decrypted);
+}
