ede157ae8ddaa1c09ee13ded37ab7bd3db9c5724
François Dupressoir <fdupress@gmail.com>
2015-08-20 12:47:08 +0200

    Entry points for HMAC-SHA512.
    Some thoughts regarding modularity in comments.

---
+++ examples/sodium/auth.c
@@ -0,0 +1,37 @@
+#include "../smack.h"
+
+#include "sodium.h"
+
+int crypto_auth_pub(unsigned char *out, const unsigned char *in,
+                    unsigned long long inlen, const unsigned char *k)
+{
+  /* Argument addresses are public for now */
+  public_in(region_of_var(out));
+  public_in(region_of_var(in));
+  public_in(region_of_var(k));
+
+  /* Annotations */
+  public_in(region_of_var(inlen));
+  public_in(mem_region(in,inlen));
+  declassified_out(mem_region(out,crypto_auth_BYTES));
+  
+
+  return crypto_auth(out,in,inlen,k);
+}
+
+int crypto_auth_sec(unsigned char *out, const unsigned char *in,
+                    unsigned long long inlen, const unsigned char *k)
+{
+  /* Argument addresses are public for now */
+  public_in(region_of_var(out));
+  public_in(region_of_var(in));
+  public_in(region_of_var(inlen));
+  public_in(region_of_var(k));
+
+  /* Annotations: everything is private */
+  /* Ideally, we would be able to provide a single set of annotations
+     that captures that the output is declassified w.r.t. the key, but
+     not the input. This is not useful for whole program analysis. */
+
+  return crypto_auth(out,in,inlen,k);
+}
