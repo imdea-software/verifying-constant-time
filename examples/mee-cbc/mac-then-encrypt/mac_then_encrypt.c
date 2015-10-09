88c213d6a239f1db46e67047445a4f503d117204
François Dupressoir <fdupress@gmail.com>
2015-10-09 11:39:57 +0200

    A simpler declassification example... hopefully

--- examples/mee-cbc/mac-then-encrypt/mac_then_encrypt.c
+++ examples/mee-cbc/mac-then-encrypt/mac_then_encrypt.c
@@ -19,22 +19,3 @@ int decrypt_then_verify(unsigned char *out,unsigned long *out_len, const unsigne
   // IMPORTANT: zero-out the output buffer if the MAC check failed (using cmove),
   // or use an internal buffer and copy it out only if the MAC check succeeds (using cmove)
 }
-
-/*
-// What we would like to do with declassification 
-int decrypt_then_verify(unsigned char *out,unsigned long *out_len, const unsigned char *in,unsigned long in_len,
-  const unsigned char *iv,const unsigned char *enc_sk,const unsigned char *mac_sk) {
-  int res;
-  res = decrypt(out,out_len,in,in_len,iv,enc_sk); 
-  res &= crypto_auth_verify(out,in_len,*out_len,mac_sk); 
-  if (res) {
-    *out_len -=*out_len;
-  }
-  else {
-    *out_len = 0;
-  }
-  return res;
-  // IMPORTANT: zero-out the output buffer if the MAC check failed (using cmove),
-  // or use an internal buffer and copy it out only if the MAC check succeeds (using cmove)
-}
-*/
---
+++ examples/mee-cbc/mee-cbc-declassify.c
@@ -0,0 +1,44 @@
+#include "../ct-verif.h"
+
+#include "crypto_hashblocks.h"
+#include "crypto_auth.h"
+#include "crypto_block.h"
+#include "pad_cbc_aes128.h"
+
+int decrypt_then_verify_declassify(unsigned char *out,unsigned long *out_len,
+      	                             const unsigned char *in,unsigned long in_len,
+      	                             const unsigned char *iv,
+      	                             const unsigned char *enc_sk,
+      	                             const unsigned char *mac_sk) {
+  int res;
+
+  res = decrypt(out,out_len,in,in_len,iv,enc_sk); 
+  res &= crypto_auth_verify(out,in_len,*out_len,mac_sk); 
+  if (res) {
+    *out_len -= *out_len; // Is this really what we want?
+  }
+  else {
+    *out_len = 0;
+  }
+  return res;
+}
+
+int dv_wrapper(unsigned char *out,unsigned long *out_len,
+                 const unsigned char *in,unsigned long in_len,
+                 const unsigned char *iv,
+                 const unsigned char *enc_sk,
+                 const unsigned char *mac_sk) {
+  __disjoint_regions(out,*out_len,in,in_len);
+  public_in_value(__SMACK_value(out));
+  public_in_value(__SMACK_value(out_len));
+  public_in_value(__SMACK_value(in));
+  public_in_value(__SMACK_value(in_len));
+  public_in_value(__SMACK_value(iv));
+  public_in_value(__SMACK_value(enc_sk));
+  public_in_value(__SMACK_value(mac_sk));
+
+  public_in_object(__SMACK_object(in,32));
+  public_in_object(__SMACK_object(iv,INPUTBYTES));
+
+  return decrypt_then_verify_declassify(out,out_len,in,in_len,iv,enc_sk,mac_sk);
+}
