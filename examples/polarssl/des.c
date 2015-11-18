10d8107a31073f6bf4906cdff16dbfd945b0b89b
François Dupressoir <fdupress@gmail.com>
2015-11-18 18:20:15 +0100

    PolarSSL example update.

--- examples/polarssl/des.c
+++ examples/polarssl/des.c
@@ -11,39 +11,39 @@ int mbedtls_des3_ENcrypt_cbc_wrapper(mbedtls_des3_context *ctx,
                                      const unsigned char *input,
                                      unsigned char *output) {
   /* Boilerplate */
-  public_in_value(__SMACK_value(ctx));
-  public_in_value(__SMACK_value(iv));
-  public_in_value(__SMACK_value(input));
-  public_in_value(__SMACK_value(output));
+  public_in(__SMACK_value(ctx));
+  public_in(__SMACK_value(iv));
+  public_in(__SMACK_value(input));
+  public_in(__SMACK_value(output));
 
   /* Useful */
-  public_in_value(__SMACK_value(length));
-  public_in_object(__SMACK_object(iv,8));
+  public_in(__SMACK_value(length));
+  public_in(__SMACK_values(iv,8));
 
-  declassified_out_object(__SMACK_object(output,16/*length*/));
-  public_out_value(__SMACK_return_value());
+  //  declassified_out(__SMACK_values(output,16/*length*/));
+  //  public_out(__SMACK_return_value());
 
   return mbedtls_des3_crypt_cbc(ctx,MBEDTLS_DES_ENCRYPT,16/*length*/,iv,input,output);
 }
 
-int mbedtls_des3_DEcrypt_cbc_wrap(mbedtls_des3_context *ctx,
+int mbedtls_des3_DEcrypt_cbc_wrapper(mbedtls_des3_context *ctx,
                                   size_t length,
                                   unsigned char iv[8],
                                   const unsigned char *input,
                                   unsigned char *output) {
   /* Boilerplate */
-  public_in_value(__SMACK_value(ctx));
-  public_in_value(__SMACK_value(iv));
-  public_in_value(__SMACK_value(input));
-  public_in_value(__SMACK_value(output));
+  public_in(__SMACK_value(ctx));
+  public_in(__SMACK_value(iv));
+  public_in(__SMACK_value(input));
+  public_in(__SMACK_value(output));
 
   /* Useful */
-  public_in_value(__SMACK_value(length));
-  public_in_object(__SMACK_object(iv,8));
+  public_in(__SMACK_value(length));
+  public_in(__SMACK_values(iv,8));
 
-  public_in_object(__SMACK_object(input,16/*length*/));
-  declassified_out_object(__SMACK_object(output,16/*length*/));
-  public_out_value(__SMACK_return_value());
+  public_in(__SMACK_values(input,16/*length*/));
+  //  declassified_out(__SMACK_object(output,16/*length*/));
+  public_out(__SMACK_return_value());
 
   return mbedtls_des3_crypt_cbc(ctx,MBEDTLS_DES_DECRYPT,16/*length*/,iv,input,output);
 }
--- examples/polarssl/sha256.c
+++ examples/polarssl/sha256.c
@@ -13,17 +13,13 @@ void mbedtls_sha256_wrapper( const unsigned char *input, size_t ilen,
   __disjoint_regions(input,ilen,output,32);
 
   /* Boilerplate */
-  public_in_value(__SMACK_value(input));
-  public_in_value(__SMACK_value(output));
+  public_in(__SMACK_value(input));
+  public_in(__SMACK_value(output));
 
   /* Useful */
-  public_in_value(__SMACK_value(ilen));
-  public_in_value(__SMACK_value(is224));
-  declassified_out_object(__SMACK_object(output,32));
-
-  //  mbedtls_sha256_init(&ctx);
-  //  mbedtls_sha256_starts(&ctx,is224);
-  //  mbedtls_sha256_update(&ctx,input,ilen);
+  public_in(__SMACK_value(ilen));
+  public_in(__SMACK_value(is224));
+  declassified_out(__SMACK_values(output,32));
 
   mbedtls_sha256(input,ilen,output,is224);
 }
